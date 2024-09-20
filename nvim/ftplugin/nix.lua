069ceffeff32ba2a054cacbb85b7a6faca369a40
GreenTheColour1 <contact@camerongreen.ca>
2024-09-20 16:45:30 -0400

    change tab for nix files

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -16,3 +16,8 @@ vim.lsp.start {
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth= 2
